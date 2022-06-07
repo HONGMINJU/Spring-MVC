32ad27a1c428b9869cc6e9ee27389b9966894302
HONGMINJU <mhong0800@gmail.com>
2022-06-07 10:15:52 +0900

    :sparkles: HTTP start line 정보 출력

---
+++ src/main/java/hello/servlet/basic/request/RequestHeaderServlet.java
@@ -0,0 +1,51 @@
+package hello.servlet.basic.request;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "requestHeaderServlet", urlPatterns = "/request-header")
+public class RequestHeaderServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        printStartLine(request);
+    }
+
+    // 1. start line 정보 출력
+    private void printStartLine(HttpServletRequest request) {
+        System.out.println("--- REQUEST-LINE - start ---");
+
+        System.out.println("request.getMethod() = " + request.getMethod()); //GET
+        System.out.println("request.getProtocol() = " + request.getProtocol()); // HTTP/1.1
+        System.out.println("request.getScheme() = " + request.getScheme()); //http
+        // http://localhost:8080/request-header
+        System.out.println("request.getRequestURL() = " + request.getRequestURL());
+        // /request-header
+        System.out.println("request.getRequestURI() = " + request.getRequestURI());
+        //username=hi
+        System.out.println("request.getQueryString() = " + request.getQueryString());
+        System.out.println("request.isSecure() = " + request.isSecure()); //https 사용 유무
+        System.out.println("--- REQUEST-LINE - end ---");
+        System.out.println();
+
+        /**
+         *        <출력 결과>
+         * --- REQUEST-LINE - start ---
+         * request.getMethod() = GET
+         * request.getProtocol() = HTTP/1.1
+         * request.getScheme() = http
+         * request.getRequestURL() = http://localhost:8080/request-header
+         * request.getRequestURI() = /request-header
+         * request.getQueryString() = username=minju
+         * request.isSecure() = false
+         * --- REQUEST-LINE - end ---
+         */
+    }
+
+}
