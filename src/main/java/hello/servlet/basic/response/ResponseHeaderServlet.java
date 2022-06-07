32c7279da5fb7820fddabe32554d17c9cf4b6a08
HONGMINJU <mhong0800@gmail.com>
2022-06-07 17:00:35 +0900

    :sparkles: header 설정 편의 메서드 적용

--- src/main/java/hello/servlet/basic/response/ResponseHeaderServlet.java
+++ src/main/java/hello/servlet/basic/response/ResponseHeaderServlet.java
@@ -2,6 +2,7 @@ package hello.servlet.basic.response;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -16,18 +17,63 @@ public class ResponseHeaderServlet extends HttpServlet {
         // [status-line]
         response.setStatus(HttpServletResponse.SC_OK);
 
-        // [response-headers]
-        // content type 설정
-        response.setHeader("Content-Type", "text/plain;charset=utf-8");
-        // cache 무효화
-        response.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
-        response.setHeader("Pragma", "no-cache");
+//        // [response-headers]
+//        // content type 설정
+//        response.setHeader("Content-Type", "text/plain;charset=utf-8");
+//        // cache 무효화
+//        response.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+//        response.setHeader("Pragma", "no-cache");
+//
+//        // 사용자 정의 헤더
+//        response.setHeader("my-header", "hello");
 
-        // 사용자 정의 헤더
-        response.setHeader("my-header", "hello");
+        // [헤더 편의 메서드]
+        content(response);
+        cookie(response);
+        redirect(response);
 
         PrintWriter writer = response.getWriter();
         writer.write("ok");
+    }
+
+    private void content(HttpServletResponse response) {
+        /**
+         *  설정하고 싶은 값
+         *  Content-Type: text/plain;charset=utf-8
+         *  Content-Length: 2
+         */
+
+        // response.setHeader("Content-Type", "text/plain;charset=utf-8");
+
+        response.setContentType("text/plain");
+        response.setCharacterEncoding("utf-8");
+        // response.setContentLength(2); //(생략시 자동 생성)
+    }
+
+    private void cookie(HttpServletResponse response) {
+        /**
+         *  설정하고 싶은 값
+         *  Set-Cookie: myCookie=good; Max-Age=600;
+         */
+
+        // response.setHeader("Set-Cookie", "myCookie=good; Max-Age=600");
+
+        Cookie cookie = new Cookie("myCookie", "good");
+        cookie.setMaxAge(600); //600초
+        response.addCookie(cookie);
+    }
+
+    private void redirect(HttpServletResponse response) throws IOException {
+        /**
+         *  설정하고 싶은 값
+         *  Status Code 302
+         *  Location: /basic/hello-form.html
+         */
+
+        // response.setStatus(HttpServletResponse.SC_FOUND); //302
+        // response.setHeader("Location", "/basic/hello-form.html");
+
+        response.sendRedirect("/basic/hello-form.html");
     }
 
 }
